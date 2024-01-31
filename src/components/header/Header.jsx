0b4718046477e0e662d2a97852a9c03502931fa2
webkopp <nakopps@googlemail.com>
2024-01-31 16:49:12 +0100

    icons-header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,10 @@ import { useContext, useRef, useState } from 'react'
 import { mainContext } from '../../context/mainProvider'
 import { Link } from 'react-router-dom'
 import "./header.css"
-import logo from "../../assets/img/image 1.png";
-import darki from "../../assets/img/mode.png";
+import logo from "../../assets/img/image 1.svg"
+import darki from "../../assets/img/mode.svg"
+import back from "../../assets/img/back.svg"
+import close from "../../assets/img/close.svg"
 
 
 
@@ -19,27 +21,38 @@ const Header = ( ) => {
   return (
 
     <>
-
-          <body style={{ backgroundColor }}>
-                <button onClick={toggleMode}>{darki}</button>
-          </body>
         <Link to="/">
             <img
               src={logo}
               alt="Pokemon-Logo"
-              style={{ width: "2rem" }}
+              style={{ width: "20%" }}
             /> 
         </Link>
+
         <nav>
+          <Link to="#">
+              <img
+                src={back}
+                alt="Back-Icon"
+              /> 
+          </Link>
             
-            <div className="search">
+          <div className="search">
             <input
-                type="text"
-                ref={searchRef}
-                placeholder="Search Pokemon"
+              type="text"
+              ref={searchRef}
+              placeholder="Search Pokemon"
+              onChange={}
             />
-            </div>
-            <button id='modeToggle' onClick={() => toggleMode()}>🌓</button>
+          </div>
+
+          <button id='modeToggle' onClick={toggleMode}>
+              <img 
+                src={darki}
+                alt="Dark-Icon"
+              />
+          </button>
+
         </nav>
         
 
