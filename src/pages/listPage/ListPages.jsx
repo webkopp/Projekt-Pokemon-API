2dee2ac07d540a9a0edc2f8fb57ff413283ad770
Kaiznkinkaide <daniel.th.cohen@gmail.com>
2024-01-31 10:20:23 +0100

    typsection

--- src/pages/listPage/ListPages.jsx
+++ src/pages/listPage/ListPages.jsx
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react'
 import PokeList from '../../components/pokeList/PokeList'
 import { mainContext } from '../../context/mainProvider'
+import BurgerMeneu from '../../components/burgermeneu/BurgerMeneu'
 
 const ListPages = () => {
 
     const {type} = useContext(mainContext)
-
-    return (
+  
+    return (<>
+        <BurgerMeneu/>
         <PokeList list={type}/>
+        </>
     )
 }
 
---
+++ src/pages/typsPage/TypsPage.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import "../typsPage/typspage.css"
+import axios from 'axios'
+
+
+
+const TypsPage = () => {
+
+
+
+  return (<>
+    <div className='typs'>
+    <h1>TypsPage</h1>
+    <button>BUG</button>
+    <button>DARK</button>
+    <button>DRAGON</button>
+    <button>ELECTRIC</button>
+    <button>FAIRY</button>
+    <button>FIGHTING</button>
+    <button>FIRE</button>
+    <button>FLYING</button>
+    <button>GHOST</button>
+    <button>GRASS</button>
+    <button>GROUND</button>
+    <button>ICE</button>
+    <button>NORMAL</button>
+    <button>PLANT</button>
+    <button>POISON</button>
+    <button>PSYCHIC</button>
+    <button>ROCK</button>
+    <button>STEEL</button>
+    <button>WATER</button>
+    </div>
+    </>
+  )
+}
+
+export default TypsPage
