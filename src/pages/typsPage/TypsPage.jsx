4ce2471c5b42b959b9743b75ba6564ff626b6db7
Kaiznkinkaide <daniel.th.cohen@gmail.com>
2024-01-31 12:01:35 +0100

    filterfunction

--- src/pages/typsPage/TypsPage.jsx
+++ src/pages/typsPage/TypsPage.jsx
@@ -1,35 +1,47 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import "../typsPage/typspage.css"
 import axios from 'axios'
+import { mainContext } from '../../context/mainProvider'
+import { Navigate, useNavigate } from 'react-router-dom'
+
 
 
 
 const TypsPage = () => {
+    const {apiData, setFilter} = useContext(mainContext)
+    const navigate = useNavigate()
 
+const getValue = (event) => {
+    const btnValue = event.target.textContent.toLowerCase()
+    console.log(btnValue);
+    setFilter(btnValue)
+    navigate("/")
+    
 
+}
 
   return (<>
     <div className='typs'>
     <h1>TypsPage</h1>
-    <button>BUG</button>
-    <button>DARK</button>
-    <button>DRAGON</button>
-    <button>ELECTRIC</button>
-    <button>FAIRY</button>
-    <button>FIGHTING</button>
-    <button>FIRE</button>
-    <button>FLYING</button>
-    <button>GHOST</button>
-    <button>GRASS</button>
-    <button>GROUND</button>
-    <button>ICE</button>
-    <button>NORMAL</button>
-    <button>PLANT</button>
-    <button>POISON</button>
-    <button>PSYCHIC</button>
-    <button>ROCK</button>
-    <button>STEEL</button>
-    <button>WATER</button>
+    <button onClick={getValue}>BUG</button>
+    <button onClick={getValue}>DARK</button>
+    <button onClick={getValue}>DRAGON</button>
+    <button onClick={getValue}>ELECTRIC</button>
+    <button onClick={getValue}>FAIRY</button>
+    <button onClick={getValue}>FIGHTING</button>
+    <button onClick={getValue}>FIRE</button>
+    <button onClick={getValue}>FLYING</button>
+    <button onClick={getValue}>GHOST</button>
+    <button onClick={getValue}>GRASS</button>
+    <button onClick={getValue}>GROUND</button>
+    <button onClick={getValue}>ICE</button>
+    <button onClick={getValue}>NORMAL</button>
+    <button onClick={getValue}>PLANT</button>
+    <button onClick={getValue}>POISON</button>
+    <button onClick={getValue}>PSYCHIC</button>
+    <button onClick={getValue}>ROCK</button>
+    <button onClick={getValue}>STEEL</button>
+    <button onClick={getValue}>WATER</button>
     </div>
     </>
   )
