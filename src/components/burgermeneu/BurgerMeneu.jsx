bde099bc31e6b2121654e7140347bd6b5c7cf676
renehellmich <renehellmich94@gmail.com>
2024-02-02 14:08:58 +0100

    add code for back-operation

--- src/components/burgermeneu/BurgerMeneu.jsx
+++ src/components/burgermeneu/BurgerMeneu.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import burgerLogo from "../../assets/img/menu.svg"
+import back from '../../assets/img/back.svg'
 
 const BurgerMeneu = () => {
+
+  const location = useLocation()
   return (
-    <Link to='/typsPage'><img src={burgerLogo} /></Link>
+    <>
+      {
+        location.pathname != '/' 
+        ? <Link to='/'><img src={back} /></Link> 
+        : <Link to='/typsPage'><img src={burgerLogo} /></Link>
+      }
+    </>
   )
 }
 
